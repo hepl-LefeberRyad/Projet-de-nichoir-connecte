24214a61df79f0733ac7ceaa221e33b3ed4fe415
JoaoSousa-max <joao.sousarocha@student.hepl.be>
2025-12-04 18:43:24 +0100

    Add files via upload

---
+++ PIR SENSOR/0.3_Test_wake_up_du_microcontr_leur_avec_led.ino
@@ -0,0 +1,35 @@
+#include "M5TimerCAM.h"
+
+const int LED_PIN = 4;      // LED externe sur GPIO4
+const int sensor_PIN = 13;  // Capteur Pir 5578 BS-162 sur GPIO13
+
+void setup() {
+    // Configurer le bouton comme entrée avec pull-up
+    pinsensor, INPUT_PULLUP);
+
+    // Autoriser le réveil par bouton AVANT toute init
+    esp_sleep_enable_ext0_wakeup((gpio_nsensor, 1); // réveil sur niveau bas
+
+    // Initialisation du TimerCAM
+    TimerCAM.begin(true);
+
+    pinMode(LED_PIN, OUTPUT);
+
+    Serial.begin(115200);
+    Serial.println("Wake up!!!");
+
+    // LED toujours allumée
+    analogWrite(LED_PIN, 255);
+    vTaskDelay(pdMS_TO_TICKS(10000)); // 10 000 ms = 10 secondes
+
+
+    // Autoriser le réveil par le capteur aussi après le mode sleep 
+    esp_sleep_enable_ext0_wakeup((gpio_nsensor, 1);
+    Serial.println("sleep!!");
+    // Mise en veille après 5 secondes
+    TimerCAM.Power.timerSleep(5);
+ 
+}
+
+void loop() {
+}
