4048b78cb5941fa77df88c5162d197615145950b
JoaoSousa-max <joao.sousarocha@student.hepl.be>
2025-12-05 15:43:42 +0100

    Add files via upload

---
+++ PIR SENSOR/0.1-Test_simple_avec_led_sensor.ino
@@ -0,0 +1,33 @@
+const int SensorPin = 13;   // Capteur Pir 5578 BS-162 
+const int ledPin = 4;       // LED à contrôler
+
+bool ledActive = false;     
+unsigned long startTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(SensorPin, INPUT);   // Capteur Pir 5578 BS-162 
+  pinMode(ledPin, OUTPUT);    // led 
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  static sensor = LOW;  
+  bool currentButtonState = digitalRead(SensorPin);
+
+  // Détection du front montant
+  if (currentButtonState == HIGH && lastsensorState == LOW && !ledActive) {
+    digitalWrite(ledPin, HIGH);
+    ledActive = true;
+    startTime = millis();
+
+    Serial.println("LED allumee !");
+  }
+
+  // Extinction après 5000 ms
+  if (ledActive && millis() - startTime >= 5000) {
+    digitalWrite(ledPin, LOW);
+    ledActive = false;
+sensor = currentButtonState;
+}
